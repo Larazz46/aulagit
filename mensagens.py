05880d3972da010845dc93ad6625e8aadbac630e
Lara Tonetti <laratonetti>
2024-12-06 19:05:49 -0300

    adicionar arquivo de mensagens

---
+++ mensagens.py
@@ -0,0 +1,4 @@
+def exibir_mensagem_inicial():
+    """Exibe uma mensagem inicial para o usuário."""
+    print("Bem-vindo ao Gerador de Tabuada!")
+    print("Você pode gerar a tabuada de qualquer número.\n")
